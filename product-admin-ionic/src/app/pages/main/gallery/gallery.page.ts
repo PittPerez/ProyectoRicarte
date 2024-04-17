d0da143666a390006c245e8995191e16f20d960f
Karina <copto3754@gmail.com>
2024-04-17 09:50:59 -0600

    Modificación de la galería

--- product-admin-ionic/src/app/pages/main/gallery/gallery.page.ts
+++ product-admin-ionic/src/app/pages/main/gallery/gallery.page.ts
@@ -11,15 +11,15 @@ import { CarritoComprasPage } from '../carrito-compras/carrito-compras.page';
 })
 export class GalleryPage {
   products = [
-    { id: 1, name: 'Pastel de Chocolate', imgUrl: 'assets/img/1.jpg', price: 50, description: 'Delicioso pastel de chocolate.' },
-    { id: 2, name: 'Pastel de Fresa', imgUrl: 'assets/img/8.jpg', price: 50, description: 'Jugoso pastel de fresa.' },
-    { id: 3, name: 'Pastel de Fresa', imgUrl: 'assets/img/3.jpg', price: 50, description: 'Jugoso pastel de fresa.' },
-    { id: 4, name: 'Pastel de Chocolate', imgUrl: 'assets/img/10.jpg', price: 50, description: 'Delicioso pastel de chocolate.' },
-    { id: 5, name: 'Pastel de Fresa', imgUrl: 'assets/img/4.jpg', price: 50, description: 'Jugoso pastel de fresa.' },
-    { id: 6, name: 'Pastel de Fresa', imgUrl: 'assets/img/11.jpg', price: 50, description: 'Jugoso pastel de fresa.' },
-    { id: 7, name: 'Pastel de Chocolate', imgUrl: 'assets/img/2.jpg', price: 50, description: 'Delicioso pastel de chocolate.' },
-    { id: 8, name: 'Pastel de Fresa', imgUrl: 'assets/img/5.jpg', price: 50, description: 'Jugoso pastel de fresa.' },
-    { id: 9, name: 'Pastel de Fresa', imgUrl: 'assets/img/6.jpg', price: 50, description: 'Jugoso pastel de fresa.' },
+    { id: 1, name: 'Pastel de Cumpleaños', imgUrl: 'assets/img/1.jpg', price: 350, description: 'Delicioso pastel de chocolate con butter cream.' },
+    { id: 2, name: 'Pastel Tradicional', imgUrl: 'assets/img/8.jpg', price: 450, description: 'Jugoso pastel sabor fresa, decorado con chantilly.' },
+    { id: 3, name: 'Pastel de Choconaranja', imgUrl: 'assets/img/3.jpg', price: 400, description: 'Pastel sabor chocolate con naranja.' },
+    { id: 4, name: 'Pastel de Quinceaños', imgUrl: 'assets/img/10.jpg', price: 1500, description: 'Pastel de chocolate de dos pisos, con hoja dorada y drip blanco.' },
+    { id: 5, name: 'Pastel de Boda', imgUrl: 'assets/img/4.jpg', price: 2500, description: 'Pastel de vainilla de 3 pisos, con toques dorados y plantas verdes.' },
+    { id: 6, name: 'Pastel de Quinceaños', imgUrl: 'assets/img/11.jpg', price: 200, description: 'Pastel de red velvet de 3 pisos con decoración en rosa y toque dorados.' },
+    { id: 7, name: 'Pastel de Cumpleaños', imgUrl: 'assets/img/2.jpg', price: 500, description: 'Pastel de fresa color blanco con perlitas.' },
+    { id: 8, name: 'Pastel de Cumpleaños', imgUrl: 'assets/img/5.jpg', price: 700, description: 'Pastel red velvet de color rojo, decorado con una flor.' },
+    { id: 9, name: 'Pastel de Cumpleaños', imgUrl: 'assets/img/6.jpg', price: 800, description: 'Pastel de vainilla con drip rosa.' },
   ];
   cartItems: any[] = [];
 
