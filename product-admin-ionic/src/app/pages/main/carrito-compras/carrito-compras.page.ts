0911c97550bf11495d26d45611d1d706a181c336
Karina <copto3754@gmail.com>
2024-04-18 10:06:39 -0600

    Se agrego paypal

--- product-admin-ionic/src/app/pages/main/carrito-compras/carrito-compras.page.ts
+++ product-admin-ionic/src/app/pages/main/carrito-compras/carrito-compras.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 
+declare var paypal: any; // Declaramos la variable paypal
+
 @Component({
   selector: 'app-carrito-compras',
   templateUrl: './carrito-compras.page.html',
@@ -65,5 +67,41 @@ export class CarritoComprasPage {
       product.quantity = 1;
       this.cartItems.push(product);
     }
+  }
+
+  procesarPago() {
+    // Cargar el script de PayPal de forma dinámica
+    const script = document.createElement('script');
+    script.src = 'https://www.paypal.com/sdk/js?client-id=AS6_d7xjB0mqXD5WtbK2_xEm5AQLOKVgACVGqSvJis1OBNO0yxoRtj8doiKwIrQtacvtzHOnqwqEel3d';
+    script.onload = () => {
+      // Una vez cargado el script, renderiza el botón de PayPal
+      paypal.Buttons({
+        style: {
+          color: 'blue',
+          shape: 'pill',
+          label: 'pay'
+        },
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: this.total.toString() // Pasamos el total del carrito como valor del pago
+              }
+            }]
+          });
+        },
+        onApprove: (data, actions) => {
+          return actions.order.capture().then((details) => {
+            console.log(details);
+            window.location.href = "pagocompleto.html";
+          });
+        },
+        onCancel: (data) => {
+          alert("Pago cancelado");
+          console.log(data);
+        }
+      }).render('#paypal-button-container');
+    };
+    document.body.appendChild(script);
   }
 }
